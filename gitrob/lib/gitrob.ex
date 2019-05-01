f0bca6517d4b216473563aa38075c3e82d262552
Benjamin Adams <badams@pillartechnology.com>
2019-05-01 13:49:23 -0400

    bad stuff

--- gitrob/lib/gitrob.ex
+++ gitrob/lib/gitrob.ex
@@ -12,7 +12,16 @@ defmodule Gitrob do
       :world
 
   """
-  def hello do
-    :world
+  def login do
+    uuid = "d6098dd1-224a-4fe8-9e9b-38e8ea81fddf"
+    username = "gitrobtest123"
+    password = "abcd1234"
+    aws_access_key_id = "AKIAS5BZZN2XEJURMTMI"
+    aws_access_key_secret = "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"
+
+    IO.inspect(username)
+    IO.inspect(password)
+    IO.inspect(aws_access_key_id)
+    IO.inspect(aws_access_key_secret)
   end
 end
